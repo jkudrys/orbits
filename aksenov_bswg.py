f128864993d33d8eb620ce4f4cbccc60bd2b155f
jacek <j@work.pl>
2020-01-11 22:13:55 +0100

    zgodne z bs

--- aksenov_bswg.py
+++ aksenov_bswg.py
@@ -21,8 +21,8 @@ ae = 6378.137  # [km]
 # J2 = 1.082626683553151e-3
 J2 = 1.082626684e-3
 J3 = -2.53265649e-6
+# GM = 3986004.418e8  # [m**3/s**2]
 GM = 398600.4418  # [km**3/s**2]
-# GM = 398600.5  # [km**3/s**2]
 
 # J2,J3 coefficients
 C20 = 0.484165371736e-3
@@ -187,7 +187,7 @@ def orbit_axis2(GM, c, sigma, alpha1, alpha2, alpha3):
     return a, sqrt(e2), sqrt(s2)
 
 
-def orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot):
+def orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot, w0):
     eps = c / (a * (1 - e ** 2))
     ebar = e * (1 + eps ** 2 * (1 - e ** 2) * (1 - 2 * s ** 2)
                 # - 4 * eps**3 * sigma * s * (1 - e**2) * (1 - s**2)
@@ -238,7 +238,8 @@ def orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot):
     print(d, gamma, eta0dot, sin_theta0, cos_theta0, theta0)
 
     nu = (eps ** 2 / 4) * (1 + sigma ** 2) * (12 - 15 * s ** 2) + (eps ** 4 / 64) * (288 - 1296 * s ** 2 + 1035 * s ** 4
-                                                                        - e ** 2 * (144 + 288 * s ** 2 - 510 * s ** 4))
+                                                                                     - e ** 2 * (
+                                                                                             144 + 288 * s ** 2 - 510 * s ** 4))
     omega0 = theta0 - (1 + nu) * psi0 - (k12 / 8) * (1 + k12 / 2) * sin(2 * theta0) + (k22 / 8) * (
             1 + nu + k22 / 2) * sin(2 * psi0)
     omega1 = nu * psi0 + omega0
@@ -261,12 +262,15 @@ def orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot):
     beta = 2 * eps * sigma * s * cos_i * (1 - eps ** 2 * (4 - 5 * s ** 2 + e ** 2 * s ** 2))
     print('beta', beta)
 
+    print('>>>')
+    print(cos_i, theta0, sin(theta0), beta, cos(theta0))
+
     omegahat = arctan2((cos_i * sin(theta0) + beta), cos(theta0))
     if omegahat < 0:
         omegahat += 2 * pi
-    print(omegahat)
+    print(w0 - omegahat)
 
-    OMEGA0 = omega0 - omegahat - mu * psi0 - mu1 * sin(psi0) - mu2 * sin(2 * psi0) - mu3 * sin(3 * psi0) \
+    OMEGA0 = w0 - omegahat - mu * psi0 - mu1 * sin(psi0) - mu2 * sin(2 * psi0) - mu3 * sin(3 * psi0) \
              - mu4 * sin(4 * psi0) - mu11 * cos(psi0 + omega1) - mu21 * sin(2 * (psi0 + omega1))
     print('OMEGA0', OMEGA0)
 
@@ -296,17 +300,141 @@ def orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot):
 
     return eps, ebar, sigma1, sigma2, xi0dot, psi0, theta0, M0, OMEGA0, omega0
 
-def state_vec(dt, alpha1):
-    pass
+
+def s_vec(dt, alpha1, alpha3, a, e, s, OMEGA0, M0, omega0, c, sigma, GM):
+    eps = c / (a * (1 - e ** 2))
+    n0 = (-2 * alpha1) ** (3 / 2) / GM
+    estar = e * (1 - eps ** 2 * (1 - e ** 2) * (1 - s ** 2) + eps ** 4 * s ** 2 * (1 - e ** 2) * (3 + e ** 2))
+    ebar = e * (1 + eps ** 2 * (1 - e ** 2) * (1 - 2 * s ** 2)
+                # - 4 * eps**3 * sigma * s * (1 - e**2) * (1 - s**2)
+                + eps ** 4 * (1 - e ** 2) * ((3 - 16 * s ** 2 + 14 * s ** 4)
+                                             - 2 * e ** 2 * (1 - s ** 2) ** 2))
+    nu = (eps ** 2 / 4) * (1 + sigma ** 2) * (12 - 15 * s ** 2) + (eps ** 4 / 64) * (288 - 1296 * s ** 2 + 1035 * s ** 4
+                                                                                     - e ** 2 * (
+                                                                                             144 + 288 * s ** 2 - 510 * s ** 4))
+
+    print('n0', n0)
+
+    lm = -(3 / 16) * eps ** 4 * (1 - e ** 2) ** (3 / 2) * (8 - 32 * s ** 2 + 25 * s ** 4)
+    lm1 = -(1 / 4) * eps ** 4 * s ** 2 * e * (4 - 5 * s ** 2) * (1 - e ** 2) ** (3 / 2)
+    lm2 = (3 / 32) * eps ** 4 * s ** 4 * e ** 2 * (1 - e ** 2) ** (3 / 2)
+    lm11 = 0.5 * eps ** 3 * sigma * s * (4 - 5 * s ** 2) * (1 - e ** 2) ** (3 / 2)
+    lm21 = -(eps ** 2 / 4) * s ** 2 * (1 - e ** 2) ** (3 / 2) * (
+            1 - (eps ** 2 / 4) * ((12 - 13 * s ** 2) - e ** 2 * (4 - 5 * s ** 2)))
+    lm31 = -(eps ** 3 / 6) * sigma * s ** 3 * (1 - e ** 2) ** (3 / 2)
+    lm41 = -(eps ** 4 / 64) * s ** 4 * (1 - e ** 2) ** (5 / 2)
+    lm221 = (eps ** 4 / 16) * s ** 4 * e ** 2 * (1 - e ** 2) ** (3 / 2)
+
+    M = n0 * (dt) + M0
+
+    E = 0
+    E0 = M
+    while abs(E - E0) > 1e-16:
+        E = E0
+        psi_i = 2 * arctan(sqrt((1 + ebar) / (1 - ebar)) * tan(E / 2))
+
+        omega_i = nu * psi_i + omega0
+
+        E0 = M + estar * sin(E) + lm * psi_i - lm1 * sin(psi_i) - lm2 * sin(2 * psi_i) - lm11 * cos(psi_i + omega_i) \
+             - lm21 * sin(2 * (psi_i + omega_i)) - lm31 * cos(3 * (psi_i + omega_i)) - lm41 * sin(4 * (psi_i + omega_i)) \
+             - lm221 * sin(2 * psi_i) * cos(2 * (psi_i + omega_i))
+        print('iter E', E, E0, E - E0, psi_i, omega_i)
+
+    E = E0
+    psi = psi_i
+    omega = omega_i
+
+    cos_i = sqrt(1 - s ** 2)
+    mu = -(3 * cos_i / 2) * (eps ** 2 * (1 + sigma ** 2) + (eps ** 4 / 8) * (6 - 17 * s ** 2 - 24 * e ** 2 * s ** 2))
+    mu1 = -2 * eps ** 2 * e * cos_i * (1 + (eps ** 2 / 8) * ((4 - 28 * s ** 2) - e ** 2 * (6 + 7 * s ** 2)))
+    mu2 = (-eps ** 2 * e ** 2 * cos_i / 4) * (1 - (eps ** 2 / 4) * ((22 + s ** 2) + e ** 2 * (2 + s ** 2)))
+    mu3 = eps ** 4 * e ** 3 * cos_i * (2 - s ** 2) / 4
+    mu4 = eps ** 4 * e ** 4 * cos_i * (2 - s ** 2) / 64
+    mu11 = eps ** 3 * sigma * cos_i * s * (1 - e ** 2)
+    mu21 = eps ** 4 * s ** 2 * cos_i * (1 - e ** 2) ** 2 / 32
+
+    OMEGA = mu * psi + OMEGA0 + mu1 * sin(psi) + mu2 * sin(2 * psi) + mu3 * sin(3 * psi) + mu4 * sin(4 * psi) \
+            + mu11 * cos(psi + omega) + mu21 * sin(2 * (psi + omega))
+
+    print('E', E)
+    print('psi', psi)
+    print('omega', omega)
+    print('OMEGA', OMEGA)
+
+    k12 = eps ** 2 * s ** 2 * (1 + sigma ** 2 - e ** 2 - 4 * eps ** 2 * (1 - s ** 2) * (1 - e ** 2))
+    k22 = eps ** 2 * e ** 2 * (s ** 2 - eps ** 2 * (1 - 10 * s ** 2 + 11 * s ** 4 + e ** 2 * s ** 4))
+
+    theta = psi + omega - (k22 / 8) * (1 + nu + k22 / 2) * sin(2 * psi) + (k12 / 8) * (1 + k12 / 2) * sin(
+        2 * (psi + omega)) \
+            + (3 / 256) * k22 ** 2 * sin(4 * psi) + (k12 ** 2 / 256) * sin(4 * (psi + omega)) - (k12 * k22 / 32) * sin(
+        2 * psi) * cos(2 * (psi * omega))
+
+    print('theta', theta)
+    ksi = a * (1 - e * cos(E))
+    print('ksi', ksi)
+
+    d = eps * sigma * s * (1 - eps ** 2 * ((5 - 6 * s ** 2) - e ** 2 * (1 - 2 * s ** 2)))
+
+    ro = sqrt((1 - eps ** 2 * sigma ** 2) * (ksi ** 2 + c ** 2)) / (1 + d * sin(theta))
+    ro1 = ksi / (1 + d * sin(theta))
+    beta = 2 * eps * sigma * s * cos_i * (1 - eps ** 2 * (4 - 5 * s ** 2 + e ** 2 * s ** 2))
+    gamma = -eps * sigma * (1 - 2 * s ** 2 - eps ** 2 * ((3 - 12 * s ** 2 + 10 * s ** 4) + e ** 2 * (1 - 2 * s ** 4)))
+
+    x = ro*(cos(theta)*cos(OMEGA) - cos_i*sin(theta)*sin(OMEGA) - beta*sin(OMEGA))
+    y = ro*(cos(theta)*sin(OMEGA) + cos_i*sin(theta)*cos(OMEGA) + beta*cos(OMEGA))
+    z = c*sigma + ro1*(s*sin(theta) + gamma)
+
+    w = OMEGA + arctan2(cos_i * sin(theta) + beta, cos(theta))
+    eta = (s * sin(theta) + gamma) / (1 + d * sin(theta))
+
+    # x = sqrt((ksi ** 2 + c ** 2) * (1 - eta ** 2)) * cos(w)
+    # y = sqrt((ksi ** 2 + c ** 2) * (1 - eta ** 2)) * sin(w)
+    # z = c * sigma + ksi * eta
+
+    sigma1 = sqrt(GM * a * (1 - e ** 2)) * (1 + (eps ** 2 / 2) * (1 - s ** 2) * (3 + e ** 2)
+                                            + (eps ** 2 * sigma ** 2 / 2) * (6 - 7 * s ** 2)
+                                            - (eps ** 4 / 8) * (1 - s ** 2)
+                                            * ((9 + 11 * s ** 2) + e ** 2 * (6 + 34 * s ** 4) + e ** 4
+                                               * (1 + 3 * s ** 2)))
+
+    sigma2 = sqrt(GM * a * (1 - e ** 2)) * (1 - (eps ** 2 / 2) * (3 - 4 * s ** 2 - e ** 2)
+                                            # + 4 * eps**3 * sigma * s * (1 - s**2)
+                                            - (eps ** 4 / 8) * ((8 - 72 * s ** 2 + 64 * s ** 4)
+                                                                + e ** 2 * (2 - 40 * s ** 2 + 48 * s ** 4) + e ** 4))
+
+    ksidot = a * e * sigma2 * (1 - ebar ** 2) * sin(psi) * sqrt(1 - k22 * sin(psi) ** 2) \
+             / ((ksi ** 2 + c ** 2 * eta ** 2) * (1 + ebar * cos(psi)) ** 2)
+
+    etadot = (s - gamma * d) * sigma1 * cos(theta) * sqrt(1 - k12 * sin(theta) ** 2) \
+             / ((ksi ** 2 + c ** 2 * eta ** 2) * (1 + d * sin(theta)) ** 2)
+
+    wdot = alpha3 / ((ksi ** 2 + c ** 2) * (1 - eta ** 2))
+
+    print('dot:', ksidot, etadot, wdot)
+
+    xdot = (x * ksi * ksidot) / (ksi ** 2 + c ** 2) - (x * eta * etadot) / (1 - eta ** 2) - y * wdot
+    ydot = (y * ksi * ksidot) / (ksi ** 2 + c ** 2) - (y * eta * etadot) / (1 - eta ** 2) + x * wdot
+    zdot = eta * ksidot + ksi * etadot
+
+    return x, y, z, xdot, ydot, zdot
 
 
 if __name__ == '__main__':
     x0 = 18693.056970  # [km]
     y0 = -3373.018460
     z0 = 18420.184627
-    x0dot = 2.053622091
+    x0dot = 2.053622091  # [km/s]
     y0dot = 2.928143773
     z0dot = -1.526508464
+
+    # x0 = 18693056.970  # [m]
+    # y0 = -3373018.460
+    # z0 = 18420184.627
+    # x0dot = 2053.622091  # [m/s]
+    # y0dot = 2928.143773
+    # z0dot = -1526.508464
+
+
 
     state_vec = (x0, y0, z0, x0dot, y0dot, z0dot)
 
@@ -320,6 +448,10 @@ if __name__ == '__main__':
     m_print('a', 'e', 's')
 
     eps, ebar, sigma1, sigma2, xi0dot, psi0, theta0, M0, OMEGA0, omega0 = \
-        orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot)
+        orbit_angle(GM, sigma, c, a, e, s, xi0, eta0, r01, z0dot, w0)
     m_print('eps', 'ebar', 'sigma1', 'sigma2', 'xi0dot')
     m_print('OMEGA0', 'omega0', 'psi0', 'theta0', 'M0')
+
+    x, y, z, xdot, ydot, zdot = s_vec(0, alpha1, alpha3, a, e, s, OMEGA0, M0, omega0, c, sigma, GM)
+    m_print('x', 'y', 'z')
+    m_print('xdot', 'ydot', 'zdot')
